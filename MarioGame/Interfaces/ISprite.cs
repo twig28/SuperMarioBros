2e9a8f0e451d25ae6011f9c7aba234fd10cc095f
twig28 <155920332+twig28@users.noreply.github.com>
2024-09-23 14:33:49 -0400

    Isprite fix

---
+++ MarioGame/Interfaces/ISprite.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarioGame
+{
+    public interface ISprite
+    {
+        void Update(GameTime gm);
+        void Draw();
+    }
+}
