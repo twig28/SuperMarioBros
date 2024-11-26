e7c2ec642d10295e0b1661d3937aebe02e48fabc
twig28 <155920332+twig28@users.noreply.github.com>
2024-11-26 11:40:27 -0500

    fixed a bug where big mario was wider than original mario

--- MarioGame/Players/PlayerSprite.cs
+++ MarioGame/Players/PlayerSprite.cs
@@ -165,13 +165,11 @@ namespace MarioGame
            
             if(Fire || Big || Star)
             {
-                rectangle = new Rectangle((int)(UPlayerPosition.X - 27), (int)(UPlayerPosition.Y - 71), 54, 96);
+                rectangle = new Rectangle((int)(UPlayerPosition.X - 20), (int)(UPlayerPosition.Y - 71), 42, 96);
             }
             else
             {
-               
                 rectangle = new Rectangle((int)(UPlayerPosition.X - 21), (int)(UPlayerPosition.Y - 24), 42,48);
-
             }
             return rectangle;
         }
